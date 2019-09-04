3ddb4eb4d63e9b6890392e16c99e906267efda48
Muhazerin <muhd.muhazerin@gmail.com>
2019-09-04 16:05:00 +0800

    Create compress2D.c

---
+++ Week 4/Practice-Questions/compress2D.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#define SIZE 100
+void compress2D(int data[SIZE][SIZE], int rowSize, int colSize);
+int main()
+{
+    int data[SIZE][SIZE];
+    int i,j;
+    int rowSize, colSize;
+
+    printf("Enter the array size (rowSize, colSize): \n");
+    scanf("%d %d", &rowSize, &colSize);
+    printf("Enter the matrix (%dx%d): \n", rowSize, colSize);
+    for (i=0; i<rowSize; i++)
+        for (j=0; j<colSize; j++)
+            scanf("%d", &data[i][j]);
+    printf("compress2D(): \n");
+    compress2D(data, rowSize, colSize);
+    return 0;
+}
+void compress2D(int data[SIZE][SIZE], int rowSize, int colSize)
+{
+    int tempArray[SIZE][SIZE], row, col, temp10Freq[SIZE][2], tempDigit, tempFreqIndex, tempArrayRow, tempArrayCol[rowSize];
+
+    if (rowSize != colSize || rowSize < 1) {
+        return;
+    }
+
+    // clear temp10Freq array by setting all -1
+    for(row = 0; row < SIZE; row++) {
+        temp10Freq[row][0] = -1;
+        temp10Freq[row][1] = -1;
+    }
+
+    tempArrayRow = 0;
+    for(row = 0; row < rowSize; row++) {
+        tempDigit = -1;
+        tempFreqIndex = 0;
+        for(col = 0; col < colSize; col++) {
+            if (tempDigit == -1) {
+                tempDigit = data[row][col];
+                temp10Freq[tempFreqIndex][0] = tempDigit;
+                temp10Freq[tempFreqIndex][1] = 1;
+                continue;
+            }
+            if (tempDigit != data[row][col]) {
+                tempFreqIndex++;
+                tempDigit = data[row][col];
+                temp10Freq[tempFreqIndex][0] = tempDigit;
+                temp10Freq[tempFreqIndex][1] = 1;
+            }
+            else {
+                temp10Freq[tempFreqIndex][1]++;
+            }
+        }
+
+        // need to save the frequency into a new tempArray row
+        col = 0;
+        for(row = 0; row <= tempFreqIndex; row++) {
+            tempArray[tempArrayRow][col++] = temp10Freq[row][0];
+            tempArray[tempArrayRow][col++] = temp10Freq[row][1];
+        }
+        tempArrayCol[tempArrayRow] = col;
+        tempArrayRow++;
+
+        // need to clear temp10Freq array
+        for(row = 0; row < SIZE; row++) {
+            temp10Freq[row][0] = -1;
+            temp10Freq[row][1] = -1;
+        }
+    }
+
+    printf("tempArrayRow: %d\n", tempArrayRow);
+    for (row = 0; row <= tempArrayRow; row++) {
+        for (col = 0; col < tempArrayCol[row]; col++) {
+            printf("%d ", tempArray[row][col]);
+        }
+        printf("\n");
+    }
+}
