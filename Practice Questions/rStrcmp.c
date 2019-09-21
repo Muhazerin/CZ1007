a5860d8dff449d4428de809971fbed0cd7f06282
Muhazerin <muhd.muhazerin@gmail.com>
2019-09-21 20:52:56 +0800

    Create rStrcmp.c

---
+++ Practice Questions/rStrcmp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define INIT_VALUE 100
+int rStrcmp(char *s1, char *s2);
+int main()
+{
+    char source[40], target[40];
+    int result = INIT_VALUE;
+
+    printf("Enter a source string: \n");
+    gets(source);
+    printf("Enter a target string: \n");
+    gets(target);
+    result = rStrcmp(source, target);
+    printf("rStrcmp(): %d", result);
+    return 0;
+}
+int rStrcmp(char *s1, char *s2)
+{
+    if (*s1 > *s2) {
+        return 1;
+    }
+    if (*s2 > *s1) {
+        return -1;
+    }
+    if (*s1 == '\0' && *s2 == '\0') {
+        return 0;
+    }
+    return 0 + rStrcmp(++s1, ++s2);
+}
